1517b7e7f7cc857e86005cd48d27a27b8f7d41fd
xxx <xxx@xxx>
2018-03-16 18:24:40 +0800

    fix

--- ubuntu_docker_install.sh
+++ ubuntu_docker_install.sh
@@ -6,6 +6,7 @@ sudo apt-get remove docker docker-engine docker.io
 
 #2.更新软件，添加Ubuntu可选内核模块，以支持AUFS(部分内核模块可能被移动到可选模块以减小安装包大小)
 sudo apt-get update
+#14.04及以下版本
 sudo apt-get install linux-image-extra-$(uname -r) linux-image-extra-virtual
 
 #3.由于apt源使用的事https传输以确保下载过程不会被篡改，因此需要安装https传输的软件包和CA证书
@@ -33,19 +34,13 @@ sudo add-apt-repository "deb [arch=amd64] https://mirrors.ustc.edu.cn/docker-ce/
 sudo apt-get update
 sudo apt-get install docker-ce
 
-#6.开启开机启动(新版本)
-#sudo systemctl enable docker
-#sudo systemctl start docker
-#ubuntu 14.04
-sudo service docker start
-
-#7.默认情况下， docker 命令会使用 Unix socket 与 Docker 引擎通讯。而只有 root 用户和docker 组的用户才可以访问 
+#6.默认情况下， docker 命令会使用 Unix socket 与 Docker 引擎通讯。而只有 root 用户和docker 组的用户才可以访问 
 #Docker 引擎的 Unix socket。出于安全考虑，一般 Linux 系统上不会直接使用 root 用户。因此，更好地做法是将需要使
 #用 docker 的用户加入 docker用户组
 sudo groupadd docker
 sudo usermod -aG docker $USER
 
-#8.配置镜像加速器
+#7.配置镜像加速器
 #ubuntu 16.04以上的版本
 #vi /etc/docker/daemon.json
 #添加以下内容
@@ -55,9 +50,17 @@ sudo usermod -aG docker $USER
 #	]
 #}
 
-#ubuntu 14.01版本
+#ubuntu 14.04版本
 #vi /etc/default/docker
 #添加以下内容
 #DOCKER_OPTS="--registry-mirror=https://registry.docker-cn.com"
 #重启docker
 #sudo service docker restart
+
+#8.开启开机启动(新版本)
+#sudo systemctl enable docker
+#sudo systemctl start docker
+#ubuntu 14.04
+sudo service docker start
+
+
